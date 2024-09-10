61fd6dfcd9effcd5531f417194f12b0f4fcb0e20
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-09-10 12:33:22 +0300

    category tasarımı tamamlandı

---
+++ Project2_EntityFrameworkDbFirstProduct/FrmCategory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project2_EntityFrameworkDbFirstProduct
+{
+    public partial class FrmCategory : Form
+    {
+        public FrmCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCategory_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCategoryId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
